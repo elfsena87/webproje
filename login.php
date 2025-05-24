b9398b21271a50ede8ea3a86ae110dd6feb9dc23
elfsena87 <elif.sonmez3@ogr.sakarya.edu.tr>
2025-05-25 02:05:14 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,14 +1,19 @@
 <?php
-$email=$_POST['email'];
-$password=$_POST['password'];
+// Kullanıcı bilgileri
+$dogru_eposta = "b2412100001@sakarya.edu.tr";
+$dogru_sifre = "b2412100001";
 
-if($email=="b241210078@sakarya.edu.tr" && $password=="b241210078")
-{
-    echo "Hoşgeldiniz $password";
-}
-else
-{
-header("Location:login.html");
-exit();
+// Formdan gelen değerler
+$email = $_POST['email'] ?? '';
+$sifre = $_POST['password'] ?? '';
+
+// Kontrol
+if ($email === $dogru_eposta && $sifre === $dogru_sifre) {
+    $kullanici_adi = explode('@', $email)[0]; // b2412100001
+    echo "Hoşgeldiniz $kullanici_adi";
+} else {
+    // Başarısızsa login sayfasına yönlendir
+    header("Location: login.html");
+    exit;
 }
 ?>
